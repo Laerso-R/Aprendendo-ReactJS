f9775a14f800381425d1dfcce9bcd18b4e56a145
Laerso Ramos <laerso.r@gmail.com>
2021-05-12 16:53:32 -0300

    Commit 01

---
+++ aprendendoreact/src/components/basicos/Primeiro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+/* function Primeiro () {
+    return (
+        <div>
+            <h1>Primeiro</h1>
+            <h2>Exemplo do primeiro componente</h2>
+        </div>
+        )
+}
+
+export default Primeiro */
+
+export default () =>
+    <>
+        <h1>Elemento Teste</h1>
+        <h2>O Elemento</h2>
+    </>
+
+// 
---
+++ aprendendoreact/src/components/basicos/Repeticao.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import products from '../../data/products'
+
+export default props => {
+    
+    function getproductsListItem () {
+        return products.map(prod=> {
+            return <li key={prod.id}>
+                {prod.id} - {prod.nome} - R$ {prod.preco}
+            </li>
+        })
+    }
+
+    return (
+        <div>
+            <h2>Repetição</h2>
+            <ul>
+                {getproductsListItem()}
+            </ul>
+        </div>
+    )
+}
